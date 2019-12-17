1e2c17e04ceb143378d15bbf8723f7ab000d97e2
Charity Bunyon <cseymou5@gmail.com>
2019-12-16 20:41:46 -0600

    added board

--- src/components/BoardsContainer/BoardsContainer.js
+++ src/components/BoardsContainer/BoardsContainer.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import authData from '../../helpers/data/authData';
 import boardData from '../../helpers/data/boardData';
 import Board from '../Board/Board';
+import BoardForm from '../BoardForm/BoardForm';
 
 
 class BoardsContainer extends React.Component {
@@ -15,6 +16,10 @@ class BoardsContainer extends React.Component {
   }
 
   componentDidMount() {
+    this.getBoards();
+  }
+
+  getBoards = () => {
     boardData.getBoardsByUid(authData.getUid())
       .then((boards) => {
         this.setState({ boards });
@@ -22,10 +27,21 @@ class BoardsContainer extends React.Component {
       .catch((errFromBoardsContainer) => console.error({ errFromBoardsContainer }));
   }
 
+
+  addBoard = (newBoard) => {
+    boardData.saveBoard(newBoard)
+      .then(() => {
+
+      })
+      .catch((errorFromSaveBoard) => console.error({ errorFromSaveBoard }));
+  }
+
   render() {
     const { setSingleBoard } = this.props;
 
-    return (<div>{this.state.boards.map((board) => (<Board key={board.id} board={board} setSingleBoard= {setSingleBoard} />))}
+    return (<div>
+      <BoardForm addBoard={this.addBoard} />
+      {this.state.boards.map((board) => (<Board key={board.id} board={board} setSingleBoard= {setSingleBoard} />))}
     </div>);
   }
 }
